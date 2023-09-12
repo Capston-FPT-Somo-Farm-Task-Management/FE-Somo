83f8bbdf318466f6ef6a2f30869f7372f93e6be7
trinh <trinhrob123@gmail.com>
2023-09-12 23:24:37 +0700

    add sidebar

---
+++ src/common/components/Sidebar/index.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { menuItem, rootSubmenuKeys } from "./sidebarData";
+import { Menu } from 'antd';
+
+function Sidebar() {
+    const [openKeys, setOpenKeys] = useState(['sub1']);
+const onOpenChange = (keys) => {
+  const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
+  if (latestOpenKey && rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+    setOpenKeys(keys);
+  } else {
+    setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+  }
+};
+  return <div className="sideBar">
+    <Menu
+      mode="inline"
+      openKeys={openKeys}
+      onOpenChange={onOpenChange}
+      style={{
+        width: 256,
+      }}
+      items={menuItem}
+    />
+  </div>;
+}
+
+export default Sidebar;
