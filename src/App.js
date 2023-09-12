60f08ca217e302f4572d5ec964b88c79d834c790
Nguyễn Minh Anh <116052485+anhnmse150302@users.noreply.github.com>
2023-09-12 18:44:16 +0700

    folder structure & update header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 
-import Header from "common/components/Header";
-import Home from "features/pages/home";
-
-
+import Header from 'common/components/Header'
+import Home from 'features/pages/home'
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter >
-      <Header/>
+      <BrowserRouter>
+        <Header />
         <Routes>
-          <Route path="/*" element={<Home/>} exact/>
+          <Route path="/*" element={<Home />} exact />
         </Routes>
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
