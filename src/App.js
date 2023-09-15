e734137efeb7493db7fb4b7c29c82139629bfbef
Nguyễn Minh Anh <116052485+anhnmse150302@users.noreply.github.com>
2023-09-15 20:21:08 +0700

    Fix route login

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import Header from "common/components/Header";
-import Home from "features/pages/home";
-import Sidebar from "common/components/Sidebar";
-import SignIn from "features/authentication/SignIn";
-import Register from "features/authentication/Register";
-import Forgot from "features/authentication/Forgot";
-import LiveStock from "features/pages/livestock";
-import Plants from "features/pages/plants";
-import Schedule from "features/pages/home/schedule";
-import Task from "features/pages/tasks";
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Header from 'common/components/Header'
+import Home from 'features/pages/home'
+import Sidebar from 'common/components/Sidebar'
+import SignIn from 'features/authentication/SignIn'
+import Register from 'features/authentication/Register'
+import Forgot from 'features/authentication/Forgot'
+import LiveStock from 'features/pages/livestock'
+import Plants from 'features/pages/plants'
+import Schedule from 'features/pages/home/schedule'
+import Task from 'features/pages/tasks'
 
 function App() {
+  const location = useLocation()
+
+  // Function to check if the current location matches certain paths
+  const ShowSidebarAndHeader = () => {
+    const { pathname } = location
+    return !['/login', '/logout'].includes(pathname)
+  }
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <div className="content-wrapper">
-          <Sidebar />
-          <Routes>
-            <Route path="/*" element={<Home />} exact />
-            <Route path="/login" element={<SignIn />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/forgot" element={<Forgot />} />
-            <Route path="/schedule" element={<Schedule />} />
-            <Route path="/task" element={<Task />} />
-            <Route path="/livestock" element={<LiveStock />} />
-            <Route path="/plants" element={<Plants />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      {ShowSidebarAndHeader() && <Header />}
+      <div className="content-wrapper">
+        {ShowSidebarAndHeader() && <Sidebar />}
+        <Routes>
+          <Route path="/*" element={<Home />} exact />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot" element={<Forgot />} />
+          <Route path="/schedule" element={<Schedule />} />
+          <Route path="/task" element={<Task />} />
+          <Route path="/livestock" element={<LiveStock />} />
+          <Route path="/plants" element={<Plants />} />
+        </Routes>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
