403242cca7a0b9d0d6feff04763ebebad26da031
Nguyễn Minh Anh <116052485+anhnmse150302@users.noreply.github.com>
2023-09-18 21:39:29 +0700

    Add Animal Group

---
+++ src/features/pages/Animals/AnimalGroup/index.jsx
@@ -0,0 +1,12 @@
+import AddAndSearchAnimalGroup from './components/AddAndSearchAnimalGroup/AddAndSearchAnimalGroup'
+import DisplayAnimalGroup from './components/DisplayAnimalGroup/DisplayAnimalGroup'
+
+const AnimalGroup = () => {
+  return (
+    <>
+      <AddAndSearchAnimalGroup />
+      <DisplayAnimalGroup />
+    </>
+  )
+}
+export default AnimalGroup
