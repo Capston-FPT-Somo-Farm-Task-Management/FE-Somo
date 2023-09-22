b581c61a825f46a0d3c6008e1bb84cc805df2ce1
trinh <trinhrob123@gmail.com>
2023-09-22 23:51:41 +0700

    css for third, second modal, complete option task, form input for third modal

--- src/features/pages/Task/components/ModalTask/components/SecondModal/index.jsx
+++ src/features/pages/Task/components/ModalTask/components/SecondModal/index.jsx
@@ -1,21 +1,75 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHorse, faSeedling, faTree, faCow } from "@fortawesome/free-solid-svg-icons";
 
 function SecondModal({ type, onOptionSelect }) {
   const handleOptionSelect = (option) => {
     onOptionSelect(option);
-  }
-  if (type === 'planting') {
+  };
+  if (type === "planting") {
     return (
-      <div>
-        <button onClick={() => handleOptionSelect("specificPlant")}> Cây trồng cụ thể</button>
-        <button onClick={() => handleOptionSelect("wholeGarden")}> Cả vườn</button>
+      <div className="second-modal-planting">
+        <div className="specific-plant">
+          <button onClick={() => handleOptionSelect("specificPlant")}>
+            <div className="button-item">
+              <div className="button-icon">
+                <FontAwesomeIcon icon={faTree} />
+              </div>
+
+              <div className="button-text">
+                <h6>Cây trồng cụ thể</h6>
+                <span>Thêm công việc cho cây trồng cụ thể</span>
+              </div>
+            </div>
+          </button>
+        </div>
+        <div className="whole-garden">
+          <button onClick={() => handleOptionSelect("wholeGarden")}>
+            <div className="button-item">
+              <div className="button-icon">
+                <FontAwesomeIcon icon={faSeedling} />
+              </div>
+
+              <div className="button-text">
+                <h6>Cả vườn</h6>
+                <span>Thêm công việc cho cả vườn</span>
+              </div>
+            </div>
+          </button>
+        </div>
       </div>
     );
-  } else if (type === 'livestock') {
+  } else if (type === "livestock") {
     return (
-      <div>
-        <button onClick={() => handleOptionSelect("specificAnimal")}> Con vật cụ thể</button>
-        <button onClick={() => handleOptionSelect("wholeBarn")}> Cả chuồng</button>
+      <div className="second-modal-livestock">
+      <div className="specific-animal">
+          <button onClick={() => handleOptionSelect("specificAnimal")}>
+            <div className="button-item">
+              <div className="button-icon">
+                <FontAwesomeIcon icon={faHorse} />
+              </div>
+
+              <div className="button-text">
+                <h6>Con vật cụ thể</h6>
+                <span>Thêm công việc cho con vật cụ thể</span>
+              </div>
+            </div>
+          </button>
+        </div>
+        <div className="whole-barn">
+          <button onClick={() => handleOptionSelect("wholeBarn")}>
+            <div className="button-item">
+              <div className="button-icon">
+                <FontAwesomeIcon icon={faCow} />
+              </div>
+
+              <div className="button-text">
+                <h6>Cả chuồng</h6>
+                <span>Thêm công việc cho cả chuồng</span>
+              </div>
+            </div>
+          </button>
+        </div>
       </div>
     );
   }
@@ -23,4 +77,4 @@ function SecondModal({ type, onOptionSelect }) {
   return null;
 }
 
-export default SecondModal
+export default SecondModal;
---
+++ src/features/pages/Task/components/ModalTask/components/ThirdModal/ThirdModalData.js
@@ -0,0 +1,189 @@
+export const areaItem = [
+  {
+    label: "Khu vực 1",
+    value: "area1",
+  },
+  {
+    label: "Khu vực 2",
+    value: "area2",
+  },
+];
+
+export const zoneItem = [
+  {
+    label: "Vùng 1",
+    value: "zone1",
+  },
+  {
+    label: "Vùng 2",
+    value: "zone2",
+  },
+];
+
+export const barnItem = [
+  {
+    label: "Chuồng 1",
+    value: "barn1",
+  },
+  {
+    label: "Chuồng 2",
+    value: "barn2",
+  },
+];
+
+export const gardenItem = [
+  {
+    label: "Vườn 1",
+    value: "garden1",
+  },
+  {
+    label: "Vườn 2",
+    value: "garden2",
+  },
+];
+
+export const animalIdItem = [
+  {
+    label: "Bò 1",
+    value: "id1",
+  },
+  {
+    label: "Bò 2",
+    value: "id2",
+  },
+];
+
+export const plantIdItem = [
+  {
+    label: "Cây Xoài 1",
+    value: "id1",
+  },
+  {
+    label: "Cây xoài 2",
+    value: "id2",
+  },
+];
+
+export const priorityItem = [
+  {
+    label: "Cao nhất",
+    value: "highest",
+  },
+  {
+    label: "Cao",
+    value: "high",
+  },
+  {
+    label: "Trung bình",
+    value: "medium",
+  },
+  {
+    label: "Thấp",
+    value: "low",
+  },
+  {
+    label: "Thấp nhất",
+    value: "lowest",
+  },
+];
+
+export const remindItem = [
+    {
+        label: "Trước 5 phút",
+        value: "5min"
+    },
+    {
+        label: "Trước 15 phút",
+        value: "15min"
+    },
+    {
+        label: "Trước 30 phút",
+        value: "30min"
+    },
+    {
+        label: "Trước 45 phút",
+        value: "45min"
+    },
+]
+
+export const repeatItem = [
+    {
+      label: "Hàng tuần",
+      value: "daily",
+    },
+    {
+      label: "Hàng tháng",
+      value: "monthly",
+    },
+    {
+      label: "Hàng năm",
+      value: "yearly",
+    },
+  ];
+
+export const taskTypeItem = [
+  {
+    label: "Cho ăn",
+    value: "Feeding",
+  },
+  {
+    label: "Tắm",
+    value: "bath",
+  },
+];
+export const workerItem = [
+  {
+    label: "Nguyễn Văn A",
+    value: "worker1",
+  },
+  {
+    label: "Nguyễn Thị B",
+    value: "worker2",
+  },
+];
+
+export const supervisorItem = [
+  {
+    label: "Nguyễn Minh Anh",
+    value: "supervisor1",
+  },
+  {
+    label: "Đỗ Hoàng Vũ",
+    value: "supervisor2",
+  },
+];
+
+export const materialItem = [
+  {
+    label: "Xẻng",
+    value: "material1",
+  },
+  {
+    label: "Cuốc",
+    value: "material2",
+  },
+];
+
+export const statusItem = [
+  {
+    label: "Đang làm",
+    value: "todo",
+  },
+  {
+    label: "Hoàn thành",
+    value: "done",
+  },
+];
+
+export const assignItem = [
+  {
+    label: "Minh Anh",
+    value: "supervisor1",
+  },
+  {
+    label: "Hoàng Vũ",
+    value: "supervisor2",
+  },
+];
+
+
