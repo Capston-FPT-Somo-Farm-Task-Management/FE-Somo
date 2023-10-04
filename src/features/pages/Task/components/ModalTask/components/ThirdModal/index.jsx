09e384d8e370de917bc5b2e21f6d2e51cd7ed90b
trinh <trinhrob123@gmail.com>
2023-10-04 15:22:18 +0700

    get field by zone

--- src/features/pages/Task/components/ModalTask/components/ThirdModal/index.jsx
+++ src/features/pages/Task/components/ModalTask/components/ThirdModal/index.jsx
@@ -21,18 +21,25 @@ import { useSelector, useDispatch } from "react-redux";
 import { getAreas } from "../../../../../../slice/area/areaSlice";
 import { getZoneByAreaPlant } from "../../../../../../slice/zone/zonePlantSlice";
 import { getZoneByAreaLivestock } from "../../../../../../slice/zone/zoneLivestockSlice";
+import { getFieldByZone } from "features/slice/field/fieldByZoneSlice";
 
 function ThirdModal({ option }) {
   const [description, setDescription] = useState("");
   const [selectedAreaId, setSelectedAreaId] = useState(null);
+  const [selectedZoneId, setSelectedZoneId] = useState(null);
 
   const area = useSelector((state) => state.area.data);
   console.log(area);
   const zonePlant = useSelector((state) => state.zonePlant.data);
   const dataPlantZone = zonePlant.data;
+
   const zoneLivestock = useSelector((state) => state.zoneLivestock.data);
   const dataLivestockZone = zoneLivestock.data;
-  console.log(dataLivestockZone);
+
+  const fieldByZone = useSelector((state) => state.fieldByZone.data);
+  const dataFieldByZone = fieldByZone.data;
+
+  console.log(dataFieldByZone);
   const { RangePicker } = DatePicker;
   const onRangeChange = (dates, dateStrings) => {
     if (dates) {
@@ -54,24 +61,114 @@ function ThirdModal({ option }) {
       dispatch(getZoneByAreaPlant(selectedAreaId));
       dispatch(getZoneByAreaLivestock(selectedAreaId));
     }
-  }, [selectedAreaId]);
+    if (selectedZoneId) {
+      dispatch(getFieldByZone(selectedZoneId));
+    }
+  }, [selectedAreaId, selectedZoneId]);
 
   const handleSelectChange = (value) => {
     setSelectedAreaId(value);
+  };
+  const handleSelectZoneChange = (value) => {
+    setSelectedZoneId(value);
   };
 
   const handleChange = (value) => {
     console.log(`selected ${value}`);
   };
 
-  
-
   if (option === "specificAnimal") {
     return (
       <Form layout="vertical" className="task-specific-animal">
         <div className="form-left">
+          <Form.Item label="Khu vực" required>
+            <Select
+              onChange={handleSelectChange}
+              placeholder="chọn khu vực"
+              options={area?.map((item) => ({
+                label: item.name,
+                value: item.id,
+              }))}
+            />
+          </Form.Item>
+          <Form.Item label="Vùng" required>
+            <Select
+              onChange={handleSelectZoneChange}
+              options={
+                Array.isArray(dataLivestockZone)
+                  ? dataLivestockZone.map((item) => ({
+                      label: item.name,
+                      value: item.id,
+                    }))
+                  : []
+              }
+            />
+          </Form.Item>
+          <Form.Item label="Chuồng">
+            <Select
+              options={
+                Array.isArray(dataFieldByZone)
+                  ? dataFieldByZone.map((item) => ({
+                      label: item.name,
+                      value: item.id,
+                    }))
+                  : []
+              }
+            />
+          </Form.Item>
+          <Form.Item label="Id con vật">
+            <Select onChange={handleChange} options={animalIdItem} />
+          </Form.Item>
+          <Form.Item label="Độ ưu tiên">
+            <Select onChange={handleChange} options={priorityItem} />
+          </Form.Item>
+          <Form.Item label="Chọn khoảng thời gian làm">
+            <RangePicker
+              showTime
+              format="DD/MM/YYYY HH:mm"
+              onChange={onRangeChange}
+              placeholder={["Ngày giờ bắt đầu", "Ngày giờ kết thúc"]}
+            />
+          </Form.Item>
+          <Form.Item label="Nhắc lại sau">
+            <Select onChange={handleChange} options={remindItem} />
+          </Form.Item>
+          <Form.Item label="Lặp lại">
+            <Select onChange={handleChange} options={repeatItem} />
+          </Form.Item>
+        </div>
+        <div className="form-right">
+          <Form.Item label="Tên công việc">
+            <Input placeholder="Nhập tên công việc" />
+          </Form.Item>
+          <Form.Item label="Loại nhiệm vụ">
+            <Select onChange={handleChange} options={taskTypeItem} />
+          </Form.Item>
+          <Form.Item label="Người thực hiện">
+            <Select onChange={handleChange} options={workerItem} />
+          </Form.Item>
+          <Form.Item label="Người giám sát">
+            <Select onChange={handleChange} options={supervisorItem} />
+          </Form.Item>
+          <Form.Item label="Dụng cụ">
+            <Select onChange={handleChange} options={materialItem} />
+          </Form.Item>
+          <Form.Item label="Mô tả">
+            <ReactQuill
+              placeholder="Thêm mô tả chi tiết cho công việc"
+              value={description}
+              onChange={setDescription}
+            />
+          </Form.Item>
+        </div>
+      </Form>
+    );
+  } else if (option === "wholeBarn") {
+    return (
+      <Form layout="vertical" className="task-whole-barn">
+        <div className="form-left">
           <div className="area">
-            <Form.Item label="Khu vực" required>
+            <Form.Item label="Khu vực">
               <Select
                 onChange={handleSelectChange}
                 placeholder="chọn khu vực"
@@ -84,102 +181,17 @@ function ThirdModal({ option }) {
           </div>
           <div className="zone">
             <Form.Item label="Vùng">
-              <Select options={
-                    Array.isArray(dataLivestockZone)
-                      ? dataLivestockZone.map((item) => ({
-                          label: item.name,
-                          value: item.id,
-                        }))
-                      : []
-                  } />
-            </Form.Item>
-          </div>
-          <div className="barn">
-            <Form.Item label="Chuồng">
-              <Select onChange={handleChange} options={barnItem} />
-            </Form.Item>
-          </div>
-          <div className="animal-id">
-            <Form.Item label="Id con vật">
-              <Select onChange={handleChange} options={animalIdItem} />
-            </Form.Item>
-          </div>
-          <div className="priority">
-            <Form.Item label="Độ ưu tiên">
-              <Select onChange={handleChange} options={priorityItem} />
-            </Form.Item>
-          </div>
-          <div className="date">
-            <Form.Item label="Chọn khoảng thời gian làm">
-              <RangePicker
-                showTime
-                format="DD/MM/YYYY HH:mm"
-                onChange={onRangeChange}
-                placeholder={["Ngày giờ bắt đầu", "Ngày giờ kết thúc"]}
+              <Select
+                onChange={handleChange}
+                options={
+                  Array.isArray(dataLivestockZone)
+                    ? dataLivestockZone.map((item) => ({
+                        label: item.name,
+                        value: item.id,
+                      }))
+                    : []
+                }
               />
-            </Form.Item>
-          </div>
-          <div className="remind">
-            <Form.Item label="Nhắc lại sau">
-              <Select onChange={handleChange} options={remindItem} />
-            </Form.Item>
-          </div>
-          <div className="repeat">
-            <Form.Item label="Lặp lại">
-              <Select onChange={handleChange} options={repeatItem} />
-            </Form.Item>
-          </div>
-        </div>
-        <div className="form-right">
-          <div className="task-name">
-            <Form.Item label="Tên công việc">
-              <Input placeholder="Nhập tên công việc" />
-            </Form.Item>
-          </div>
-          <div className="task-type">
-            <Form.Item label="Loại nhiệm vụ">
-              <Select onChange={handleChange} options={taskTypeItem} />
-            </Form.Item>
-          </div>
-          <div className="worker">
-            <Form.Item label="Người thực hiện">
-              <Select onChange={handleChange} options={workerItem} />
-            </Form.Item>
-          </div>
-          <div className="supervisor">
-            <Form.Item label="Người giám sát">
-              <Select onChange={handleChange} options={supervisorItem} />
-            </Form.Item>
-          </div>
-          <div className="material">
-            <Form.Item label="Dụng cụ">
-              <Select onChange={handleChange} options={materialItem} />
-            </Form.Item>
-          </div>
-          <div className="description">
-            <Form.Item label="Mô tả">
-              <ReactQuill
-                placeholder="Thêm mô tả chi tiết cho công việc"
-                value={description}
-                onChange={setDescription}
-              />
-            </Form.Item>
-          </div>
-        </div>
-      </Form>
-    );
-  } else if (option === "wholeBarn") {
-    return (
-      <Form layout="vertical" className="task-whole-barn">
-        <div className="form-left">
-          <div className="area">
-            <Form.Item label="Khu vực">
-              <Select onChange={handleChange} options={areaItem} />
-            </Form.Item>
-          </div>
-          <div className="zone">
-            <Form.Item label="Vùng">
-              <Select onChange={handleChange} />
             </Form.Item>
           </div>
           <div className="barn">
@@ -363,12 +375,28 @@ function ThirdModal({ option }) {
         <div className="form-left">
           <div className="area">
             <Form.Item label="Khu vực">
-              <Select onChange={handleChange} options={areaItem} />
+              <Select
+                onChange={handleSelectChange}
+                options={area?.map((item) => ({
+                  label: item.name,
+                  value: item.id,
+                }))}
+              />
             </Form.Item>
           </div>
           <div className="zone">
             <Form.Item label="Vùng">
-              <Select onChange={handleChange} options={zoneItem} />
+              <Select
+                onChange={handleChange}
+                options={
+                  Array.isArray(dataPlantZone)
+                    ? dataPlantZone.map((item) => ({
+                        label: item.name,
+                        value: item.id,
+                      }))
+                    : []
+                }
+              />
             </Form.Item>
           </div>
           <div className="barn">
