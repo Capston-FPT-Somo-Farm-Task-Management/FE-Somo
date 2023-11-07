793544e8ac721c57bb105e85d690f57b8aaa9c56
Nguyễn Minh Anh <116052485+anhnmse150302@users.noreply.github.com>
2023-11-07 21:56:38 +0700

    get and delete animal group

---
+++ src/features/pages/Admin/AnimalGroup/TableAnimalGroup.jsx
@@ -0,0 +1,55 @@
+import { Badge, Button, Popconfirm, Table } from 'antd'
+import Column from 'antd/es/table/Column'
+
+export const TableAnimalGroup = ({ fieldByFarm, onFinishDelete }) => {
+  return (
+    <Table
+      dataSource={fieldByFarm ? fieldByFarm.data : null}
+      rowKey="id"
+      locale={{ emptyText: 'Chưa có chuồng nào' }}
+    >
+      <Column
+        title="Tên chuồng"
+        dataIndex="name"
+        key="1"
+        render={(text) => <h4>{text}</h4>}
+      />
+      <Column title="Mã chuồng" dataIndex="code" key="2" />
+      <Column title="Diện tích" dataIndex="area" key="3" />
+      <Column title="Vùng" dataIndex="zoneName" key="4" />
+      <Column title="Khu vực" dataIndex="areaName" key="5" />
+
+      <Column
+        title="Trạng thái"
+        dataIndex="isDelete"
+        key="6"
+        render={(isDelete) =>
+          isDelete === false ? (
+            <Badge status="success" text="Active" />
+          ) : (
+            <Badge status="error" text="Inactive" />
+          )
+        }
+      />
+
+      <Column
+        title="Xoá"
+        key="6"
+        dataIndex="id"
+        render={(_, record) => (
+          <Popconfirm
+            title="Xoá vườn"
+            description="Bạn có chắc muốn xoá vườn này?"
+            onConfirm={() => onFinishDelete(record.id)}
+            okText="Xoá"
+            cancelText="Huỷ"
+          >
+            <Button size="middle" danger>
+              Xoá
+            </Button>
+          </Popconfirm>
+        )}
+      />
+    </Table>
+  )
+}
