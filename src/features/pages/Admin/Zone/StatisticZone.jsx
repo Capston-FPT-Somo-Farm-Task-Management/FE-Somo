67d2899ce802d59bcbffb084abe6a7f7453f9ebd
Nguyễn Minh Anh <116052485+anhnmse150302@users.noreply.github.com>
2023-11-07 21:55:59 +0700

    get and delete zone

--- src/features/pages/Admin/Zone/StatisticZone.jsx
+++ src/features/pages/Admin/Zone/StatisticZone.jsx
@@ -1,12 +1,40 @@
-import ChartZone from './ChartZone'
+import { Divider } from 'antd'
+import DisplayCard from './DisplayCard'
 import TableZone from './TableZone'
+import { useDispatch, useSelector } from 'react-redux'
+import { getZoneByFarmId } from 'features/slice/zone/zoneByFarmSlice'
+import { getMemberById } from 'features/slice/user/memberSlice'
+import { useEffect } from 'react'
+import { authServices } from 'services/authServices'
+import { adminDeleteZone } from 'features/slice/zone/zoneSlice'
 
 const StatisticZone = () => {
+  const dispatch = useDispatch()
+  const member = useSelector((state) => state.member.data)
+  const farmId = member.farmId
+  const zoneByFarm = useSelector((state) => state.zoneByFarm.data)
+
+  useEffect(() => {
+    dispatch(getMemberById(authServices.getUserId()))
+    dispatch(getZoneByFarmId(farmId))
+  }, [dispatch])
+
+  const onFinishDelete = (id) => {
+    dispatch(adminDeleteZone(id)).then(() => {
+      loadData()
+    })
+  }
+
+  const loadData = () => {
+    dispatch(getZoneByFarmId(farmId))
+  }
   return (
-    <div>
-      <ChartZone />
-      <TableZone />
-    </div>
+    <>
+      <DisplayCard />
+      <Divider dashed />
+      <TableZone zoneByFarm={zoneByFarm} onFinishDelete={onFinishDelete} />
+    </>
   )
 }
+
 export default StatisticZone
--- src/features/pages/Admin/Zone/TableZone.jsx
+++ src/features/pages/Admin/Zone/TableZone.jsx
@@ -1,4 +1,51 @@
-const TableZone = () => {
-  return <div>TableZone</div>
+import { Badge, Button, Popconfirm, Table } from 'antd'
+import Column from 'antd/es/table/Column'
+
+const TableZone = ({ zoneByFarm, onFinishDelete }) => {
+  return (
+    <>
+      <Table
+        rowKey="id"
+        dataSource={zoneByFarm ? zoneByFarm.data : null}
+        locale={{ emptyText: 'Chưa có vùng nào' }}
+      >
+        <Column title="Tên vùng" dataIndex="name" key="1" />
+        <Column title="Mã vùng" dataIndex="code" key="2" />
+        <Column title="Diện tích" dataIndex="farmArea" key="3" />
+        <Column title="Loại vùng" dataIndex="zoneTypeName" key="4" />
+        <Column title="Tên khu vực" dataIndex="areaName" key="5" />
+        <Column
+          title="Trạng thái"
+          dataIndex="status"
+          key="6"
+          render={(status) =>
+            status === 'Active' ? (
+              <Badge status="success" text="Active" />
+            ) : (
+              <Badge status="error" text="Inactive" />
+            )
+          }
+        />
+        <Column
+          title="Xoá"
+          key="7"
+          dataIndex="id"
+          render={(_, record) => (
+            <Popconfirm
+              title="Xoá vùn"
+              description="Bạn có chắc muốn xoá vùng này?"
+              onConfirm={() => onFinishDelete(record.id)}
+              okText="Xoá"
+              cancelText="Huỷ"
+            >
+              <Button size="middle" danger>
+                Xoá
+              </Button>
+            </Popconfirm>
+          )}
+        />
+      </Table>
+    </>
+  )
 }
 export default TableZone
