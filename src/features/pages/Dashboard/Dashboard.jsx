29ff762c9337d80be0608d4d4fcdc9b8b8e5df73
trinh <trinhrob123@gmail.com>
2023-11-28 22:55:25 +0700

    fix

--- src/features/pages/Dashboard/Dashboard.jsx
+++ src/features/pages/Dashboard/Dashboard.jsx
@@ -1,53 +1,60 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { Divider } from 'antd'
-import ChartTaskWeek from './ChartTaskWeek'
-import PieChartTaskWeek from './PieChartTaskWeek'
-import { getMemberById } from 'features/slice/user/memberSlice'
-import { getTaskByWeek } from 'features/slice/task/taskByWeekSlice'
-import { authServices } from 'services/authServices'
+import React, { useEffect, useRef, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Divider } from "antd";
+import ChartTaskWeek from "./ChartTaskWeek";
+import PieChartTaskWeek from "./PieChartTaskWeek";
+import { getMemberById } from "features/slice/user/memberSlice";
+import { getTaskByWeek } from "features/slice/task/taskByWeekSlice";
+import { authServices } from "services/authServices";
+import DashboardBox from "./DashboardBox";
 
 const Dashboard = () => {
-  const barChartRef = useRef(null)
-  const dispatch = useDispatch()
-  const member = useSelector((state) => state.member.data)
-  const taskByWeek = useSelector((state) => state.taskByWeek.data)
-  const memberId = member?.id
+  const barChartRef = useRef(null);
+  const dispatch = useDispatch();
+  const member = useSelector((state) => state.member.data);
+  const taskByWeek = useSelector((state) => state.taskByWeek.data);
+  const memberId = member?.id;
 
-  const [selectedDay, setSelectedDay] = useState(null)
+  const [selectedDay, setSelectedDay] = useState(null);
 
   useEffect(() => {
-    dispatch(getMemberById(authServices.getUserId()))
-    dispatch(getTaskByWeek(memberId))
-  }, [dispatch, memberId])
+    dispatch(getMemberById(authServices.getUserId()));
+    dispatch(getTaskByWeek(memberId));
+  }, [dispatch, memberId]);
 
   const handleBarClick = (index) => {
-    setSelectedDay(index)
-  }
+    setSelectedDay(index);
+  };
 
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (barChartRef.current && !barChartRef.current.contains(event.target)) {
-        setSelectedDay(null)
+        setSelectedDay(null);
       }
-    }
+    };
 
-    document.addEventListener('mousedown', handleClickOutside)
+    document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [barChartRef])
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [barChartRef]);
 
   return (
-    <>
-      <PieChartTaskWeek taskByWeek={taskByWeek} selectedDay={selectedDay} />
-      <Divider dashed />
-      <div ref={barChartRef}>
-        <ChartTaskWeek taskByWeek={taskByWeek} onBarClick={handleBarClick} />
+    <div className="dashboard">
+      <div className="dashboard-header">
+        <DashboardBox />
       </div>
-    </>
-  )
-}
+      <div className="dashboard-footer">
+        <div className="dashboard-chart" ref={barChartRef}>
+          <ChartTaskWeek taskByWeek={taskByWeek} onBarClick={handleBarClick} />
+        </div>
+        <div className="dashboard-piechart">
+          <PieChartTaskWeek taskByWeek={taskByWeek} selectedDay={selectedDay} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
